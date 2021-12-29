d2a3da1133b1ee742645ef7c7c54f864450eb057
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2021-12-29 12:40:17 +0500

    sms is started

--- app/utilities/models.py
+++ app/utilities/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+SMS_CHOICES = (
+    (1, 'create_user'),
+    (2, 'reset_password'),
+    (3, 'notification'),
+    (4, 'news'),
+)
+
+SENT_OPTIONS = (
+    (0, 'not_sent'),
+    (1, "sent")
+)
+
+
+class SMS(models.Model):
+    phone_number = models.CharField(null=True, max_length=255)
+    text = models.CharField(null=True, max_length=255)
+    sms_type = models.PositiveSmallIntegerField(choices=SMS_CHOICES, default=1)
+    is_sent = models.PositiveSmallIntegerField(choices=SENT_OPTIONS, default=0)
---
+++ main.py
@@ -0,0 +1,4 @@
+import random
+
+random_number = random.randrange(10000, 99999)
+print(random_number)
