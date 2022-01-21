ae136e893019590b3701cb14ad901f6648ef5061
shaxzod9806 <90208087+shaxzod9806@users.noreply.github.com>
2022-01-21 15:27:16 +0500

    fixed pagination

--- app/motochas/settings.py
+++ app/motochas/settings.py
@@ -66,9 +66,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # "PAGE_SIZE": 3,
+    "PAGE_SIZE": 10,
     # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-
 }
 
 SIMPLE_JWT = {
