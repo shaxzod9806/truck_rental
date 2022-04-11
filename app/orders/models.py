6e82c412ea6367886e22086ee1e247f466b39722
shaxzod9806 <90208087+shaxzod9806@users.noreply.github.com>
2022-04-11 13:20:45 +0500

    fixed firebase_notification error

--- app/orders/models.py
+++ app/orders/models.py
@@ -57,11 +57,10 @@ class FireBaseNotification(models.Model):
     )
     title = models.CharField(max_length=255)
     body = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,default=None)
     type_notification = models.IntegerField(choices=TYPE_CHOICES, default=3)
-    oreder_id = models.IntegerField(null=True, blank=True)
+    oreder_id = models.IntegerField(null=True, blank=True, default=None)
     image_url = models.CharField(max_length=255, null=True)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
